52c478a24dad942fdb4a950d8772ba3f70f8d516
Vlas-Omsk <Vlas-Omsk@users.noreply.github.com>
2020-09-02 02:23:27 +0600

    Update

---
+++ js/pages.js
@@ -0,0 +1,19 @@
+function PageExists(page)
+{
+	try {
+		var http = new XMLHttpRequest();
+		http.open('HEAD', 'frames/' + page + '.html', false);
+		http.send();
+		return http.status!=404;
+	} catch {
+		const pages = [
+			"default",
+			"how_to_use_pictures"
+		];
+		return pages.includes(page);
+	}
+}
+
+function OpenRoot() {
+	top.window.location.href = '/';
+}
